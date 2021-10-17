d1c32034caa436c6e7d0df17b4a8a6eddb31dfb5
souvik <souvikkar0101@gmail.com>
2021-10-17 23:28:22 +0530

    custom auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function index(){
+        return view('auth.login');
+    }
+
+    public function login(Request $request){
+        //dd($request->all());
+        //valid data
+        $request->validate([
+            'email'=> 'required',
+            'password'=> 'required',
+        ]);
+
+        //login code
+        if(\Auth::attempt($request->only('email','password'))){
+            return redirect('home');
+
+        }
+        return redirect('login')->withError('Login Details are not valid...');
+    }
+
+
+
+    public function register_view(){
+        return view('auth.register');
+    }
+
+     public function register(Request $request){
+        //dd($request->all());
+
+        $request->validate([
+            'name'=> 'required',
+            'email'=> 'required|unique:users|email',
+            'password'=>'required|confirmed'
+        ]);
+
+        //save the user table
+
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>\Hash::make($request->password),
+
+        ]);
+
+        // login user here
+
+        if(\Auth::attempt($request->only('email','password'))){
+            return redirect('home');
+        }
+
+        //error massage
+
+        return redirect('register')->withError('Error');
+    }
+
+     public function home(){
+        return view('home');
+    }
+
+     public function logout(){
+
+        \Session::flush();
+        \Auth::logout();
+
+        return redirect('');
+
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+
+
+  
+@extends('layouts.app')
+
+@section('content')
+
+
+<h1>Home : {{ Auth::user()->name }}</h1>
+
+
+@endsection
